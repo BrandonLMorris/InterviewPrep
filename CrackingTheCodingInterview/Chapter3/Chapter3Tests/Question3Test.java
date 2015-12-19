de5c4e28f1832f4d555a4ff2fb6f1e8f5d77e927
brandonlmorris <brandon.morris95@gmail.com>
2015-12-19 10:22:03 -0600

    Complete chapter 3 question 3

--- CrackingTheCodingInterview/Chapter3/Chapter3Tests/Question3Test.java
+++ CrackingTheCodingInterview/Chapter3/Chapter3Tests/Question3Test.java
@@ -1,13 +1,28 @@
 package CrackingTheCodingInterview.Chapter3.Chapter3Tests;
 
+import CrackingTheCodingInterview.Chapter3.Question3;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class Question3Test {
+  Question3<Integer> q3;
 
   @Before
   public void setUp() throws Exception {
+    q3 = new Question3<Integer>();
+    for (int i = 0; i <= 100; i++) {
+      q3.push(i);
+    }
+  }
 
+  @Test
+  public void testStackSet() throws Exception {
+    assertEquals(100, (int)q3.peek());
+    assertEquals(100, (int)q3.pop());
+    assertEquals(99, (int) q3.peek());
+    assertEquals(true, q3.push(10));
+    assertEquals(10, (int) q3.peek());
   }
 }
--- CrackingTheCodingInterview/Chapter3/Question3.java
+++ CrackingTheCodingInterview/Chapter3/Question3.java
@@ -1,7 +1,57 @@
 package CrackingTheCodingInterview.Chapter3;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
 /**
- * Created by bmorris on 10/2/15.
+ * Chapter 3 Question 3
+ *
+ * Implement a data structure SetOfStacks composed of several stacks and should
+ * create a new stack once the previous one exceeds capacity. All operations
+ * (push, pop, peek) should work as expected with a normal stack.
+ *
+ * Created by Brandon Morris on 10/2/15.
  */
-public class Question3 {
+public class Question3<T> {
+  public static final int MAX_STACK_SIZE = 100;
+
+  LinkedList<LinkedList<T>> stackSet;
+  int currentSize;
+
+  public Question3() {
+    stackSet = new LinkedList<LinkedList<T>>();
+    stackSet.addFirst(new LinkedList<T>());
+    currentSize = 0;
+  }
+
+  public boolean push(T element) {
+    if (currentSize == MAX_STACK_SIZE) {
+      stackSet.push(new LinkedList<T>());
+      currentSize = 0;
+    }
+
+    stackSet.peek().addFirst(element);
+    currentSize++;
+
+    return true;
+  }
+
+  public T pop() {
+    if (stackSet.size() == 1 && currentSize == 0) {
+      throw new NoSuchElementException();
+    }
+
+    T element = stackSet.peek().pop();
+    currentSize--;
+    if (currentSize == 0 && stackSet.size() > 1) {
+      stackSet.pop();
+      currentSize = MAX_STACK_SIZE;
+    }
+
+    return element;
+  }
+
+  public T peek() {
+    return stackSet.peek().peek();
+  }
 }
